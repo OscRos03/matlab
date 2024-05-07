f8e55f84b2ca3d3a379b523f93f5708ee27fb180
OscRos03 <oscros03@gmail.com>
2024-05-07 15:21:43 +0200

    finally fixed

--- TMA044/matlab4c.m
+++ TMA044/matlab4c.m
@@ -29,9 +29,11 @@ end
 
 %%
 clear, clf
+hold on
 
 syms x y
 f_ = x .^ 3 + 0.3 .* x .^ 2 + 3.2 .* x .* y + y .^ 3 - 1.1 .* y;
+%f_ = -1 .* ( x .^ 2 + y .^ 2);
 f = matlabFunction(f_);
 fWrapper = @(s) f(s(1), s(2));
 
@@ -46,29 +48,33 @@ y = linspace(-5, 5, 30);
 [X, Y] = meshgrid(x, y);
 Z = f(X, Y);
 subplot(2, 1, 1)
+hold on
 surf(x, y, Z);
+plot3(-1.2910, -1.3205, f(-1.2910, -1.3205), 'ro')
+%plot3(0, 0, f(0, 0), 'ro')
 subplot(2, 1, 2)
 hold on
 contour(X, Y, Z, 10)
+plot(-1.2910, -1.3205, 'ro')
+%plot(0, 0, 'ro')
 
 x = [-1 -1];
 kmax = 999;
 tol = 0.5*10^-4;
 for k=1:kmax
-    plot(x(1), x(2), 'ro')
-    grad = normalize(J(x(1), x(2)));
-    g = @(s) fWrapper(x - s * grad');
+    %grad = normalize(J(x(1), x(2)));
+    grad = J(x(1), x(2));
+    g = @(s) -1 .* fWrapper(x + s * grad');
     alpha = fminbnd(g, 0, 1);         % Hur långt i gradientens riktning skall jag gå för minsta funktionsvärde?
     subplot(2, 1, 1)
     hold on
     plot3(x(1), x(2), f(x(1), x(2)), 'ro')
-    hold off
     subplot(2, 1, 2)
-    hold on
+    plot(x(1), x(2), 'ro')
     plot([x(1) x(1)+grad(1) * alpha], [x(2) x(2)+grad(2) * alpha])
-    hold off
     
     x2 = x;
+    x
     x = x + alpha * grad';
     g(alpha)
     n = norm(x2 - x)
